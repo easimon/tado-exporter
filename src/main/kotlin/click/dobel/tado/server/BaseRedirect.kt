033dc92d9f957037c9917c73f373dbc7eb78aa6f
Markus Dobel <markus.dobel@senacor.com>
2020-02-24 09:58:26 +0100

    change base url redirect to a temporary one

--- src/main/kotlin/click/dobel/tado/server/BaseRedirect.kt
+++ src/main/kotlin/click/dobel/tado/server/BaseRedirect.kt
@@ -4,13 +4,12 @@ import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import java.net.URI
-import javax.inject.Singleton
 
 @Controller
 class BaseRedirect {
 
   @Get("/")
   fun root(): HttpResponse<String> {
-    return HttpResponse.redirect(URI("/prometheus"))
+    return HttpResponse.temporaryRedirect(URI("/prometheus"))
   }
 }

73693855af2ac18365b47bf76b450faa3a1c7fd7
Markus Dobel <markus.dobel@senacor.com>
2020-04-18 10:52:42 +0200

    refactor annotation metadata extensions test to an integration test

---
+++ tado-util/src/test/kotlin/click/dobel/tado/util/AnnotationMetadataExtensionsIntegrationTest.kt
@@ -0,0 +1,37 @@
+package click.dobel.tado.util
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.micronaut.context.ApplicationContext
+import io.micronaut.test.annotation.MicronautTest
+import javax.inject.Singleton
+
+@MicronautTest
+internal class AnnotationMetadataExtensionsTest(
+  context: ApplicationContext
+) : StringSpec({
+
+  "stringValue translates to java class" {
+    val beanDefinition = context.getBeanDefinition(TestAnnotatedClass::class.java)
+
+    beanDefinition.stringValue(TestAnnotation::class, VALUE) shouldBe "test"
+  }
+
+  "stringValues translates to java class" {
+    val beanDefinition = context.getBeanDefinition(TestAnnotatedClass::class.java)
+
+    beanDefinition.stringValues(TestAnnotation::class, VALUES) shouldBe arrayOf("test1", "test2")
+  }
+})
+
+private const val VALUE = "value"
+private const val VALUES = "values"
+
+internal annotation class TestAnnotation(
+  val value: String,
+  val values: Array<String>
+)
+
+@Singleton
+@TestAnnotation(value = "test", values = ["test1", "test2"])
+internal class TestAnnotatedClass

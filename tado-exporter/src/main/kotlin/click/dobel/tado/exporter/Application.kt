ae9815ee49a3f78f9365aafdf9ab990b96c33949
Markus Dobel <markus.dobel@senacor.com>
2020-04-19 14:43:25 +0200

    register shutdown signal handlers

--- tado-exporter/src/main/kotlin/click/dobel/tado/exporter/Application.kt
+++ tado-exporter/src/main/kotlin/click/dobel/tado/exporter/Application.kt
@@ -1,11 +1,18 @@
 package click.dobel.tado.exporter
 
 import io.micronaut.runtime.Micronaut
+import sun.misc.Signal
+import kotlin.system.exitProcess
 
 object Application {
 
+  private const val SIGINT = "INT"
+  private const val SIGTERM = "TERM"
+
   @JvmStatic
   fun main(args: Array<String>) {
+    Signal.handle(Signal(SIGINT)) { _ -> exitProcess(0) }
+    Signal.handle(Signal(SIGTERM)) { _ -> exitProcess(1) }
     Micronaut.build()
       .packages("click.dobel.tado")
       .mainClass(Application.javaClass)

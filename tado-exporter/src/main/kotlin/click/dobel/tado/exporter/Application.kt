bda2e584d558c9926762510320efd51154a9534e
Markus Dobel <markus.dobel@senacor.com>
2021-03-21 16:31:24 +0100

    make application main() a kotlin function

--- tado-exporter/src/main/kotlin/click/dobel/tado/exporter/Application.kt
+++ tado-exporter/src/main/kotlin/click/dobel/tado/exporter/Application.kt
@@ -1,21 +1,17 @@
 package click.dobel.tado.exporter
 
-import io.micronaut.runtime.Micronaut
+import io.micronaut.runtime.Micronaut.build
 import sun.misc.Signal
 import kotlin.system.exitProcess
 
-object Application {
+private val SHUTDOWN_SIGNALS = listOf("INT", "TERM")
 
-  private val SHUTDOWN_SIGNALS = listOf("INT", "TERM")
-
-  @JvmStatic
-  fun main(args: Array<String>) {
-    SHUTDOWN_SIGNALS.forEach { signal ->
-      Signal.handle(Signal(signal)) { _ -> exitProcess(0) }
-    }
-    Micronaut.build()
-      .packages("click.dobel.tado")
-      .mainClass(Application.javaClass)
-      .start()
+fun main(args: Array<String>) {
+  SHUTDOWN_SIGNALS.forEach { signal ->
+    Signal.handle(Signal(signal)) { exitProcess(0) }
   }
+  build()
+    .args(*args)
+    .packages("click.dobel.tado")
+    .start()
 }
